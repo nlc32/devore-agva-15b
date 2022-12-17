91b8df866bf9e19ac6675a06a04d60c9b3db8edb
Glitch (dark-mousy-college) <none>
2022-12-17 02:34:00 +0000

    🐆🌓 Checkpoint ./home.html:47975725/1648 ./function.js:47975725/368 ./style.css:47975725/195

--- function.js
+++
@@ -1,8 +0,0 @@
-function blockads() {
-  var x = document.getElementById("special-offer");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
